843f6f8cb73dfcc3e89735403a407328f75bf2e5
srinivas-hub-alt <111947882+srinivas-hub-alt@users.noreply.github.com>
2022-12-27 10:48:45 +0530

    crud finish but rough

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,28 @@ function parseName(name){
   return name.split(' ').map(word=>word.charAt(0).toUpperCase()+word.slice(1)).join(' ')
 }
 
-function createNewItem(){
-  console.log('athalaam innum panala');
-}
+
+
+
 
 function App() {
 
   const [testData, setTestData] = React.useState([]);
+  const [itemData, setItemData] = React.useState({'name':'','price':'','category':'','_id':''});
+  const [pageState, setPageState] = React.useState('View'); // tracks create state very bad naming :(
+
+  const fetchData = useCallback(()=>{
+    fetch('/api/v1/items/').then(res=>{
+      return res.json()
+    }).then(data=>{
+      console.log(data);
+      setTestData(data.items);
+    }).catch(err=>{console.log(err);})
+  },[]);
+
+  function createButtonClicked(){
+    setPageState('Create')
+  }
 
   const deleteCurrentItem = useCallback((id)=>
     {
@@ -21,34 +36,87 @@ function App() {
         return res.json()
       }).then(data=>{
         console.log(data);
+        fetchData();
       })
-    }, []
+    }, [fetchData]
   );
 
-  React.useEffect(()=>{
-    fetch('/api/v1/items/').then(res=>{
+
+  const createItem = useCallback((pageState)=>
+  {
+    console.log(pageState)
+    if(pageState==='Create')
+    {
+      console.log(itemData)
+      fetch('/api/v1/items/', {method: 'POST', headers: {
+        'Content-Type': 'application/json'
+      },body: JSON.stringify(itemData)}).then(result=>result.json).then(data=>{console.log(data); fetchData()})
+    }
+    else{
+      console.log(itemData)
+      fetch(`/api/v1/items/${itemData._id}`, {method: 'PATCH', headers: {
+        'Content-Type': 'application/json'
+      },body: JSON.stringify(itemData)}).then(result=>result.json).then(data=>{console.log(data); fetchData()})
+    }
+  }, [fetchData, itemData] 
+  );
+
+  function updateItemSetup(_id)
+  {
+    setPageState('Update');
+    fetch(`/api/v1/items/${_id}`).then(res=>{
       return res.json()
     }).then(data=>{
       console.log(data);
-      setTestData(data.items);
+      setItemData({'name':data.items.name,'price':data.items.price,'category':data.items.category, '_id':_id})
     }).catch(err=>{console.log(err);})
-  },[deleteCurrentItem])
+    
+  }
+
+  function formDataChange(event)
+  {
+    const {name, value} = event.target;
+    console.log(value)
+    setItemData(prevData => ({...prevData, [name]:value}))
+    console.log(itemData)
+  }
+
+  React.useEffect(()=>{
+    fetchData()
+  },[fetchData, deleteCurrentItem])
 
 
   const allDataFetched = testData.map(item=>{
     return ( 
-        <Item deleteItem={deleteCurrentItem} parseName={parseName} {...item}/>
+        <Item updateItemSetup={updateItemSetup} deleteItem={deleteCurrentItem} parseName={parseName} {...item}/>
     )
   })
 
   return (
     <div className="App">
-      <div className = "ItemList">
-          {!allDataFetched ? 'Loading data...': allDataFetched}
-      </div>
-      <button className='Item--new' onClick={createNewItem}>
-        Create New Item
-      </button>
+      {pageState === 'View' && 
+      <>
+        <div className = "ItemList">
+            {!allDataFetched ? 'Loading data...': allDataFetched}
+        </div>
+        <button className='Item--new' onClick={createButtonClicked}>
+          Create New Item
+        </button>
+      </>
+      }
+      {(pageState === 'Create' || pageState === 'Update') && 
+      <>
+        <form onSubmit={()=>createItem(pageState)}>
+            <label htmlFor='Name'>Name</label>
+            <input type='text' name='name' id='Name' value={itemData.name} onChange={(event)=>formDataChange(event)}/>
+            <label htmlFor='Price'>Price</label>
+            <input type='text' name='price' id='Price' value={itemData.price} onChange={(event)=>formDataChange(event)}/>
+            <label htmlFor='Category'>Category</label>
+            <input type='text' name='category' id='Category' value={itemData.category} onChange={(event)=>formDataChange(event)}/>
+            <button>{pageState === 'Create'?'Add':'Update'}</button>
+        </form>
+      </>}
+      
     </div>
   );
 }
--- client/src/Item/Item.js
+++ client/src/Item/Item.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export default function Item(props){
-    const {deleteItem, parseName, _id, name, price, category} = props;
+    const {updateItemSetup, deleteItem, parseName, _id, name, price, category} = props;
     console.log(parseName(name), price, category, _id)
     return (
         <div className='each--item'>
             <ul>
                 <button className='each--item--delete' onClick={()=>deleteItem(_id)} >Delete</button>
+                <button className='each--item--delete' onClick={()=>updateItemSetup(_id)} >Update</button>
                 <li>{parseName(name)}</li>
                 <li><span className=''><span className='each--item--price--label'>Price: </span>{price}</span></li>
                 <li><span className=''><span className='each--item--category--label'>Category: </span>{category}</span></li>
